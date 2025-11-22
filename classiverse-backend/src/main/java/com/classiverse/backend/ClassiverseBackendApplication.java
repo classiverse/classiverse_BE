0bfb834595cf0ed2b484067b6bba6a7861b49cd8
geunmoon <geunmooon@gmail.com>
2025-11-22 16:17:27 +0900

    feat: spring.io setting

---
+++ classiverse-backend/src/main/java/com/classiverse/backend/ClassiverseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.classiverse.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassiverseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassiverseBackendApplication.class, args);
+	}
+
+}
---
+++ classiverse-backend/src/test/java/com/classiverse/backend/ClassiverseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classiverse.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassiverseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
